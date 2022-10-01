27d7012a51d17743623bd5775064d4627d33a9b1
XMochina <nathanmatthewchen@gmail.com>
2022-10-01 09:34:42 -0400

    Added comments, edited colours, and moved the new JButtons outside

--- src/NumPuzzle.java
+++ src/NumPuzzle.java
@@ -30,27 +30,33 @@ import javax.swing.JTextField;
 import javax.swing.Timer;
 import javax.swing.text.html.ImageView;
 
+
 public class NumPuzzle extends WindowAdapter implements ActionListener {
 	
-	private JFrame frame;//main frame
+	private JFrame frame; // main frame
 
 	
-	//panels that I need
+	/* Panels for the puzzle and options */
 	private JPanel playingPane,rightPane;
 	
-	/*Font*/
+	
+	/* Size and font of the buttons and options */
 	Font modeFont = new Font("Sansserif", Font.BOLD, 35);
 	Font buttonFont = new Font("Arial", Font.PLAIN, 50);
 	
-	/*buttons that is needed*/
-	JButton[] btnArray;
 	
+	JButton[] btnArray; // Array of buttons
+	
+	
+	/* Buttons needed for the puzzle */
 	JButton btn1;JButton btn2;JButton btn3;JButton btn4;JButton btn5;
 	JButton btn6;JButton btn7;JButton btn8;JButton btn9;JButton btn10;
 	JButton btn11;JButton btn12;JButton btn13;JButton btn14;JButton btn15;
 	JButton btn16;JButton btn17;JButton btn18;JButton btn19;JButton btn20;
 	JButton btn21;JButton btn22;JButton btn23;JButton btn24;JButton btn25;
 	
+	
+	/* Buttons needed for the options */
 	JButton startButton = new JButton("Start");
 	JButton restartButton = new JButton("Restart");
 	JButton clearButton = new JButton("Clear");
@@ -61,55 +67,50 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 	JButton finButton = new JButton("Finish");
 	
 	
-	
-	/*Menu variables*/
-	private JMenu menu;
-	private JMenuItem gameItem, helpItem;
-	
+	/* Menu variables */
+//	private JMenu menu;
+//	private JMenuItem gameItem, helpItem;
 	
 	
-	/*Label variables*/
-	private JLabel timeLabel,scoreLabel,logoLabel,iconLabel;
+	/* Label variables */
+	private JLabel iconLabel;
+//	private JLabel timeLabel,scoreLabel,logoLabel;
 	
 	
-	
-//	/*check box for mode*/
+//	/* check box for mode */
 //	private Checkbox modeCheckBox = new Checkbox("Mode :");
 	
 	
-	
-	
-	/*Options for game*/
+	/* Options for game */
 	private String sizeOptions[] = 	{"2x2","3x3","4x4","5x5"};	
 	private String typeOption[] = {"Text","Num"};
 	
 	
-	/*Combobox for type options, game options*/
-	private JComboBox typeOptionList;
-	private JComboBox gameOptionsList;
+	/* Combobox for type options, game options */
+	private JComboBox<String> typeOptionList;
+	private JComboBox<String> gameOptionsList;
 	
-	/*Variable*/
+	
+	/* Variables for the dimensions */
 	private int dimSize;
 	private String gameSize;
 	
-	/*Radio button*/
+	
+	/* Radio button for the options */
 	private JRadioButton r1;
 	private JRadioButton r2;
 	
 	
-	/*text field for time and point*/
+	/* Text field for time and point */
 	private JTextField timeDisplay;
 	private JTextField pointDisplay;
 	
 	
-	
-	/*text field for history(log)*/
+	/* Text field for history(log) */
 	private JTextArea logArea;
 	
 	
-	
-	
-	/*method to start*/
+	/* Method to start */
 	public NumPuzzle() {
 		setAndLaunch();
 		puzzleDimension(gameSize, dimSize);
@@ -119,58 +120,95 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 	/*launch*/
 	private void setAndLaunch() {
 		
+		
+		/* Logo icon and size */
 		ImageIcon logoIcon = new ImageIcon("Logo.png");
 		iconLabel = new JLabel();
 		iconLabel.setIcon(logoIcon);
 		iconLabel.setBounds(50,30,300,60);
+		iconLabel.setBackground(new Color(203,208,204));
 		timeDisplay = new JTextField();
 		timeDisplay.setFont(modeFont);
 		
 		
-		frame = new JFrame("Fun Number Puzzle made by Jaeho Oh and Nathan Chen");//title
+		frame = new JFrame("Fun Number Puzzle made by Jaeho Oh and Nathan Chen"); // Title
 		
 		
-		/*center panel for playing*/
-//		playingPane = new JPanel();
-//		playingPane.setBackground(new Color(203,208,204));
-//
-//		playingPane.setBounds(100,100,500,500);
-//		
-//		
-//		playingPane.setLayout(new GridLayout(3,3,4,4));
-//		
-//		playingPane.setBounds(10,10,650,650);
+		/* Center panel for playing */
+		playingPane = new JPanel();
+//		playingPane.setBounds(100,100,500,500);		
+		playingPane.setBounds(10,10,650,650);
+		playingPane.setBackground(new Color(203,208,204));
 
 		
-		/*panel that is on right side for the components*/
+		/* Right panel for components */
 		rightPane = new JPanel();
 		rightPane.setLayout(new BorderLayout());
 		
-		rightPane.add(iconLabel);
+		
+		rightPane.add(iconLabel); // Add icon to right pane
 		
 		
-		/*adding panels to frame*/
+		/* Add panels to the frame */
 		frame.add(rightPane);
-//		frame.add(playingPane);
-
+		frame.add(playingPane);
+		
 		
 		rightPane.setBounds(670,0,400,850);
-		rightPane.setBackground(Color.yellow);
+		rightPane.setBackground(new Color(231,235,218));
 		rightPane.add(clearButton);
 
 		clearButton.setBounds(650,400,10,10);
-
 		clearButton.setBounds(140,600,100,50);
+		clearButton.setBackground(new Color(228,160,016));
 
 		
-		/*option list for the size of the game*/
+		/* Option list (drop-down) for the dimensions of the game */
 		gameOptionsList = new JComboBox<String>(sizeOptions);
-		//System.out.println(gameOptionsList.getSelectedItem());
+
 		DefaultListCellRenderer centerRenderer = new DefaultListCellRenderer();
 		centerRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER);
 		gameOptionsList.setFont(modeFont);
-		gameOptionsList.setRenderer(centerRenderer);
+		//gameOptionsList.setRenderer(centerRenderer);
 		
+		/* Add buttons for the center panel */
+		btn1 = new JButton("1");
+		btn1.setBackground(new Color(228,160,016));
+		btn2 = new JButton("2");
+		btn2.setBackground(new Color(228,160,016));
+		btn3 = new JButton("3");
+		btn3.setBackground(new Color(228,160,016));
+		btn4 = new JButton("4");
+		btn4.setBackground(new Color(228,160,016));
+		btn5 = new JButton("5");
+		btn5.setBackground(new Color(228,160,016));
+		btn6 = new JButton("6");
+		btn6.setBackground(new Color(228,160,016));
+		btn7 = new JButton("7");
+		btn7.setBackground(new Color(228,160,016));
+		btn8 = new JButton("8");
+		btn8.setBackground(new Color(228,160,016));
+		btn9 = new JButton("9");
+		btn9.setBackground(new Color(228,160,016));
+		btn10 = new JButton("10");
+		btn10.setBackground(new Color(228,160,016));
+		btn11 = new JButton("11");
+		btn11.setBackground(new Color(228,160,016));
+		btn12 = new JButton("12");
+		btn12.setBackground(new Color(228,160,016));
+		btn13 = new JButton("13");
+		btn13.setBackground(new Color(228,160,016));
+		btn14 = new JButton("14");
+		btn14.setBackground(new Color(228,160,016));
+		btn15 = new JButton("15");
+		btn15.setBackground(new Color(228,160,016));
+		btn16 = new JButton("16");
+		btn16.setBackground(new Color(228,160,016));
+		btn25 = new JButton(" ");
+		btn25.setBackground(new Color(203,208,204));		
+		
+		
+		/* Actions */
 		ActionListener cbActionListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -183,9 +221,6 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 				//System.out.println(dimSize);
 				
 				puzzleDimension(gameSize, dimSize);
-				
-				
-				playingPane.repaint();
 			}
 		};
 		
@@ -204,17 +239,18 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 		dimTitle.setBounds(60,115,180,40);
 		
 		gameOptionsList.setBounds(160,115,180,40);
+		gameOptionsList.setBackground(new Color(203,208,204));
 		
 		JLabel dummyLabel = new JLabel(); 
 		
 		
-		/*Mode option*/
+		/* Mode option */
 		r1 = new JRadioButton("Design");
 		r2 = new JRadioButton("Play");
 		r1.setBounds(160,160,80,60);
 		r2.setBounds(240,160,50,60);
-		r1.setBackground(Color.yellow);
-		r2.setBackground(Color.yellow);
+		r1.setBackground(new Color(231,235,218));
+		r2.setBackground(new Color(231,235,218));
 		r1.setMnemonic(KeyEvent.VK_D);
 		r2.setMnemonic(KeyEvent.VK_P);
 		JLabel modeTitle = new JLabel("Mode: ");
@@ -227,34 +263,38 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 		rightPane.add(r1);rightPane.add(r2);
 		
 		
-		
-		/*Buttons*/
+		/* Button options for the right panel */
 		saveButton.setBounds(55,230,80,50);
+		saveButton.setBackground(new Color(228,160,016));
 		rightPane.add(saveButton);
 		
 		loadButton.setBounds(150,230,80,50);
+		loadButton.setBackground(new Color(228,160,016));
 		rightPane.add(loadButton);
 		
 		randButton.setBounds(245,230,80,50);
+		randButton.setBackground(new Color(228,160,016));
 		rightPane.add(randButton);
 		
 		startButton.setBounds(55,300,125,50);
+		startButton.setBackground(new Color(228,160,016));
 		rightPane.add(startButton);
 		
 		hideButton.setBounds(200,300,125,50);
+		hideButton.setBackground(new Color(228,160,016));
 		rightPane.add(hideButton);
 		
 		
-		
 		/*type selection here*/
-		typeOptionList = new JComboBox(typeOption);
+		typeOptionList = new JComboBox<String>(typeOption);
 		typeOptionList.setFont(new Font("Sansserif", Font.BOLD, 15));
 		typeOptionList.setRenderer(centerRenderer);
 		typeOptionList.setBounds(100,380,70,30);
+		typeOptionList.setBackground(new Color(203,208,204));
 		rightPane.add(typeOptionList);
 		
 		
-		
+		/* Type font and size */
 		JLabel typeTitle = new JLabel("Type : ");
 		typeTitle.setFont(new Font("Sansserif", Font.BOLD, 15));
 		typeTitle.setBounds(45, 380, 55, 30);
@@ -271,7 +311,10 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 		pointDisplay.setEditable(false);
 		
 		timeDisplay.setBounds(100,420,70,30);
+		timeDisplay.setBackground(new Color(203,208,204));
 		pointDisplay.setBounds(100,460,70,30);
+		pointDisplay.setBackground(new Color(203,208,204));
+		
 		
 		JLabel timeTitle = new JLabel("Time");
 		JLabel pointTitle = new JLabel("Point");
@@ -296,8 +339,9 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 		/*log history area*/
 		logArea = new JTextArea("Your move");		
 		logArea.setBounds(200,380,130,150);
+		logArea.setBackground(new Color(203,208,204));
 		logArea.setEditable(false);
-		logArea.setBackground(Color.lightGray);
+		logArea.setBackground(new Color(203,208,204));
 		logArea.setFont(new Font("Sansserif", Font.BOLD, 15));
 		rightPane.add(logArea);
 	
@@ -321,52 +365,34 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 	}
 	
 	private void puzzleDimension(String gameSize, int dimSize) {
-		/*center panel for playing*/
-		playingPane = new JPanel();
-		playingPane.setBackground(new Color(203,208,204));
-		playingPane.setBounds(100,100,500,500);
-		playingPane.setLayout(new GridLayout(3,3,4,4));
-		playingPane.setBounds(10,10,650,650);
-		
-		frame.add(playingPane);
-		
-		//System.out.println(gameSize);
-		//System.out.println(dimSize);
+		playingPane.removeAll();
 		
 		
-		btnArray = new JButton[25];
-		
-		
-		btnArray[0] = btn1;
-		btnArray[1] = btn2;
-		btnArray[2] = btn3;
-		btnArray[3] = btn4;
-		btnArray[4] = btn5;
-		btnArray[5] = btn6;
-		btnArray[6] = btn7;
-		btnArray[7] = btn8;
-		btnArray[8] = btn9;
-
-			
-		JButton btn1 = new JButton("1");
-		JButton btn2 = new JButton("2");
-		JButton btn3 = new JButton("3");
-		JButton btn4 = new JButton("0");
-		JButton btn5 = new JButton("5");
-		JButton btn6 = new JButton("6");
-		JButton btn7 = new JButton("7");
-		JButton btn8 = new JButton("8");
-		JButton btn9 = new JButton(" ");
-
-
+		/* The dimensions for the puzzle in the center panel */
 		switch(dimSize) {
-			case 2:
+			case 2: // If the dimension is 2x2
+				playingPane.revalidate();
 				playingPane.add(btn1);
 				playingPane.add(btn2);
 				playingPane.add(btn3);
-				playingPane.add(btn9);
+				playingPane.add(btn25);
+				playingPane.setLayout(new GridLayout(2,2,4,4));
 				break;
-			case 3:
+			case 3: // If the dimension is 3x3
+				playingPane.revalidate();
+				playingPane.add(btn1);
+				playingPane.add(btn2);
+				playingPane.add(btn3);
+				playingPane.add(btn4);
+				playingPane.add(btn5);
+				playingPane.add(btn6);
+				playingPane.add(btn7);
+				playingPane.add(btn8);
+				playingPane.add(btn25);
+				playingPane.setLayout(new GridLayout(3,3,4,4));
+				break;
+			case 4: // If the dimension is 4x4
+				playingPane.revalidate();
 				playingPane.add(btn1);
 				playingPane.add(btn2);
 				playingPane.add(btn3);
@@ -376,8 +402,16 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 				playingPane.add(btn7);
 				playingPane.add(btn8);
 				playingPane.add(btn9);
+				playingPane.add(btn10);
+				playingPane.add(btn11);
+				playingPane.add(btn12);
+				playingPane.add(btn13);
+				playingPane.add(btn14);
+				playingPane.add(btn15);
+				playingPane.add(btn25);
+				playingPane.setLayout(new GridLayout(4,4,4,4));
 				break;
-			default:
+			default: // Dimension is 3x3 by default
 				playingPane.add(btn1);
 				playingPane.add(btn2);
 				playingPane.add(btn3);
@@ -386,125 +420,12 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 				playingPane.add(btn6);
 				playingPane.add(btn7);
 				playingPane.add(btn8);
-				playingPane.add(btn9);
+				playingPane.add(btn25);
+				playingPane.setLayout(new GridLayout(3,3,4,4));
+				break;
 		}
-//		
-//		JButton btn1 = new JButton("1");
-//		JButton btn2 = new JButton("2");
-//		JButton btn3 = new JButton("3");
-//		
-//		btn1.setFont(buttonFont);
-//		btn2.setFont(buttonFont);
-//		btn3.setFont(buttonFont);
-//		btn1.addActionListener(this);  
-//		btn2.addActionListener(this);  
-//		btn3.addActionListener(this);  
-//		
-//		playingPane.add(btn1);playingPane.add(btn2);playingPane.add(btn3);
-//		btnArray[0] = btn1;
-//		btnArray[1] = btn2;
-//		btnArray[2] = btn3;
-//		
-//		if(dimSize >2) {
-//			JButton btn4 = new JButton("4");
-//			JButton btn5 = new JButton("5");
-//			JButton btn6 = new JButton("6");
-//			JButton btn7 = new JButton("7");
-//			JButton btn8 = new JButton("8");
-//				
-//			btn4.setFont(buttonFont);
-//			btn5.setFont(buttonFont);
-//			btn6.setFont(buttonFont);
-//			btn7.setFont(buttonFont);
-//			btn8.setFont(buttonFont);
-//			btn4.addActionListener(this);  
-//			btn5.addActionListener(this);  
-//			btn6.addActionListener(this);  
-//			btn7.addActionListener(this);  
-//			btn8.addActionListener(this); 
-//			
-//			playingPane.add(btn4);playingPane.add(btn5);playingPane.add(btn6);
-//			playingPane.add(btn7);playingPane.add(btn8);
-//			btnArray[3] = btn4;
-//			btnArray[4] = btn5;
-//			btnArray[5] = btn6;
-//			btnArray[6] = btn7;
-//			btnArray[7] = btn8;
-//		}
-//		if(dimSize >3) {
-//			JButton btn9  = new JButton("9");
-//			JButton btn10 = new JButton("10");
-//			JButton btn11 = new JButton("11");
-//			JButton btn12 = new JButton("12");
-//			JButton btn13 = new JButton("13");
-//			JButton btn14 = new JButton("14");
-//			JButton btn15 = new JButton("15");
-//			
-//			btn9.setFont(buttonFont);
-//			btn10.setFont(buttonFont);
-//			btn11.setFont(buttonFont);
-//			btn12.setFont(buttonFont);
-//			btn13.setFont(buttonFont);
-//			btn14.setFont(buttonFont);
-//			btn15.setFont(buttonFont);
-//			btn9.addActionListener(this);
-//			
-//			
-//			playingPane.add(btn9);playingPane.add(btn10);playingPane.add(btn11);
-//			playingPane.add(btn12);playingPane.add(btn13);playingPane.add(btn14);
-//			playingPane.add(btn15);
-//			
-//			btnArray[8] = btn9;
-//			btnArray[9] = btn10;
-//			btnArray[10] = btn11;
-//			btnArray[11] = btn12;
-//			btnArray[12] = btn13;
-//			btnArray[13] = btn14;
-//			btnArray[14] = btn15;
-//		}
-//		if(dimSize >4) {
-//			JButton btn16 = new JButton("16");
-//			JButton btn17 = new JButton("17");
-//			JButton btn18 = new JButton("18");
-//			JButton btn19 = new JButton("19");
-//			JButton btn20 = new JButton("20");
-//			JButton btn21 = new JButton("21");
-//			JButton btn22 = new JButton("22");
-//			JButton btn23 = new JButton("23");
-//			JButton btn24 = new JButton("24");
-//			
-//			
-//			btn16.setFont(buttonFont);
-//			btn17.setFont(buttonFont);
-//			btn18.setFont(buttonFont);
-//			btn19.setFont(buttonFont);
-//			btn20.setFont(buttonFont);
-//			btn21.setFont(buttonFont);
-//			btn22.setFont(buttonFont);
-//			btn23.setFont(buttonFont);
-//			btn24.setFont(buttonFont);
-//			
-//			playingPane.add(btn16);playingPane.add(btn17);playingPane.add(btn18);
-//			playingPane.add(btn19);playingPane.add(btn20);playingPane.add(btn21);
-//			playingPane.add(btn22);playingPane.add(btn23);playingPane.add(btn24);
-//		
-//			btnArray[15] = btn16;
-//			btnArray[16] = btn17;
-//			btnArray[17] = btn18;
-//			btnArray[18] = btn19;
-//			btnArray[19] = btn20;
-//			btnArray[20] = btn21;
-//			btnArray[21] = btn22;
-//			btnArray[22] = btn23;
-//			btnArray[23] = btn24;
-//		}
-//			
-//		JButton btn25 = new JButton(" ");
-//		btnArray[btnArray.length-1] = btn25;
-		
-		
-//		playingPane.setLayout(new GridLayout(dimSize,dimSize,4,4));
 	}
+	
 	
 	private Timer timer = new Timer(1000, new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
@@ -513,6 +434,8 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 		}
 	});
 	
+	
+	/* Score and seconds variables */
 	private int score;
 	private int seconds = 0;
 	private boolean started = false;
@@ -520,13 +443,8 @@ public class NumPuzzle extends WindowAdapter implements ActionListener {
 	private String score_String = String.format("%d", score);
 	
 	
-	
 	ImageIcon logo = new ImageIcon("Logo.gif");
 	
-	
-	
-	
-
 	
 	public static void main(String[] args) {
 		new NumPuzzle();
